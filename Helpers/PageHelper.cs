293ef9a195d0a1ad46193bca98e6e86f33471b5f
Bhbrk <bogdankrut12@gmail.com>
2021-11-30 19:25:09 +0200

    commenting and pagination v2

---
+++ Helpers/PageHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Helpers
+{
+    public static class PageHelper
+    {
+        public static IEnumerable<double> optimizedPageNumbers(double PageNumber, double PagesCount)
+        {
+
+            List<double> optimizedPaginationPages = new List<double>();
+            if (PagesCount <= 5)
+            {
+                for (double i = 1; i <= PagesCount; i++)
+                {
+                    optimizedPaginationPages.Add(i);
+                }
+            }
+            else
+            {
+                double midPoint = PageNumber < 3 ? 3 : PageNumber > PagesCount - 2 ? PagesCount - 2 : PageNumber;
+
+                for (double i = midPoint - 1; i <= midPoint + 1; i++)
+                {
+                    optimizedPaginationPages.Add(i);
+                }
+
+                if (optimizedPaginationPages[0] != 1)
+                {
+                    optimizedPaginationPages.Insert(0, 1);
+
+                    if (optimizedPaginationPages[1] - optimizedPaginationPages[0] > 1)
+                    {
+                        optimizedPaginationPages.Insert(1, -1);
+                    }
+                }
+
+                if (optimizedPaginationPages[optimizedPaginationPages.Count - 1] != PagesCount)
+                {
+                    optimizedPaginationPages.Insert(optimizedPaginationPages.Count, PagesCount);
+                    if (optimizedPaginationPages[optimizedPaginationPages.Count - 1] - optimizedPaginationPages[optimizedPaginationPages.Count - 2] > 1)
+                    {
+                        optimizedPaginationPages.Insert(optimizedPaginationPages.Count - 1, -1);
+                    }
+                }
+            }
+
+
+
+            return optimizedPaginationPages;
+        }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication5.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Username{ get; set; }
+        public string Email{ get; set; }
+        public string Address{ get; set; }
+        public string Note{ get; set; }
+        [DataType(DataType.Password)]
+        public string Password{ get; set; }
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
